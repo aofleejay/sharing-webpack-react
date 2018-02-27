fb4bb2e9b781664f35f84fddbb51e9f82c4fd40e
kunapot <kunapot@thinknet.co.th>
2018-02-27 17:38:04 +0700

    Setup React project with hot loader

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env', 'babel-preset-react'],
+            plugins: ['react-hot-loader/babel']
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
